0077508cf0a05dd2c40c5c7e93f2260f3279f177
Kaysha Griffin <gri22027@byui.edu>
2023-01-04 16:29:50 -0700

    James Bond naming system

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("What is your first name? ");
         string FirstName = Console.ReadLine();
-        string LastName = Console.ReadLine();
 
-        Console.Write("What is your first name?");
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}." );
     }
