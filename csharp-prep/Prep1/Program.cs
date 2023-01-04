a8b33793a5ac411c089ec1d59cc1a0c906a22a62
Kaysha Griffin <gri22027@byui.edu>
2023-01-04 16:20:00 -0700

    added name and removed 'this is the starter code line'

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string FirstName = Console.ReadLine();
+        string LastName = Console.ReadLine();
+
+        Console.Write("What is your first name?");
+        Console.Write("What is your last name?");
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}." );
     }
 }
