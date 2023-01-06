d17d76ea0664677804132face6971ab07f96fe9e
Kaysha Griffin <gri22027@byui.edu>
2023-01-05 20:22:32 -0700

    Grade and Passing

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write ("Enter your grade ");
+        string grade = Console.ReadLine();
+        int Grade = int.Parse(grade);
+
+       if (Grade >= 90)
+       {
+        Console.WriteLine("You got an A.");
+       }
+       else if(Grade >= 80)
+       {
+        Console.WriteLine("You got a B.");
+       }
+       else if (Grade >= 70)
+       {
+        Console.WriteLine("You got a C.");
+       }
+       else if (Grade >= 60)
+       {
+        Console.WriteLine("You got a D.");
+       }
+       else
+       {
+        Console.WriteLine("You got an F.");
+       }
+
+       string passing = Console.ReadLine();
+       int Passing = int.Parse(passing);
+       string TextVersion = passing.ToString();
+       if (Passing >= 70)
+       {
+        Console.WriteLine("You passed!");
+       }
+       else
+       {
+        Console.WriteLine("You didn't pass. You can do it next time!");
+       }
     }
 }
