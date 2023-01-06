cfd2c7ae393ac632e96a00f503159f9b6009effa
Kaysha Griffin <gri22027@byui.edu>
2023-01-05 21:40:56 -0700

    Number Guessing

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        
+        //Console.Write($"What is the magic number ? {number}");
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the number!");
+            }
+        }
+
     }
 }
