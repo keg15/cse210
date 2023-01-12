e7c7eade640a150f21e4ad4e12097fc08d8d3987
Kaysha Griffin <gri22027@byui.edu>
2023-01-11 17:15:24 -0700

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,12 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Welcome to the program!");
+        welcome();
+        string name = inputName();
+        int number = inputNumber();
+        int squaredNumber = inputSquaredNumber(number);
+        result(name, squaredNumber);
+    }
 
-        static void displayName (string name)
-        {
-            Console.WriteLine($"Please enter your name: {name}");
-            return;
-        }
+    static void welcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+static string inputName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int inputNumber()
+    {
+        Console.Write("Please enter your favortie number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int inputSquaredNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void result(string name, int square)
+    {
+        Console.Write($"{name} the square root of your number is {square}.");
     }
 }
