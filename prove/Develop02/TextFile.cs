c3a2a12412ae5ecfec7899605d029912a083bbbd
Kaysha Griffin <gri22027@byui.edu>
2023-01-21 18:56:49 -0700

    Entries

--- prove/Develop02/TextFile.cs
+++ prove/Develop02/TextFile.cs
@@ -1,9 +1,9 @@
 using System.IO;
 
-//string fileName = "myJournal.txt";
-//using (StreamWriter outputFile = new StreamWriter(filename))
+string fileName = "myJournal.txt";
+using (StreamWriter outputFile = new StreamWriter(fileName))
 {
 }
 
-//string filename = "myJournal.txt";
-//string[] lines = System.IO.File.ReadAllLines(filename);
+string filename = "myJournal.txt";
+string[] lines = System.IO.File.ReadAllLines(filename);
