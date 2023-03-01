8f6820589d8a47dae469bd35b839d10319d5318c
Kaysha Griffin <gri22027@byui.edu>
2023-02-28 22:21:11 -0700

    Shapes

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,20 @@
+public abstract class Shape
+{
+    string _color;
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
